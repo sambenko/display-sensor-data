574942e8519a5073ff3587df02b738cddee7141b
sambenko <sam.benko@gmail.com>
2023-05-23 18:15:08 +0200

    working esp-mbedtls connection

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ use embedded_graphics::{
     pixelcolor::Rgb565, prelude::*,
 };
 
+use embedded_io::blocking::*;
+use embedded_svc::{
+    ipv4::Interface,
+    wifi::{ClientConfiguration, Configuration, Wifi},
+};
+
 use esp32s3_hal::{
     clock::{ClockControl, CpuClock},
     gpio::IO,
@@ -17,8 +23,12 @@ use esp32s3_hal::{
     spi
 };
 
-use esp_wifi::esp_now::{PeerInfo, BROADCAST_ADDRESS};
-use esp_wifi::{current_millis, initialize};
+use esp_wifi::{
+    current_millis,
+    initialize,
+    wifi::{utils::create_network_interface, WifiMode},
+    wifi_interface::WifiStack,
+};
 
 use esp_backtrace as _;
 use esp_println::println;
@@ -27,6 +37,19 @@ use display_interface_spi::SPIInterfaceNoCS;
 use mipidsi::{ColorOrder, Orientation};
 
 use ui::{ build_ui, update_temperature };
+
+use smoltcp::{
+    iface::SocketStorage, 
+    wire::{ IpAddress, Ipv4Address }
+};
+use esp_mbedtls::{set_debug, Mode, TlsVersion};
+use esp_mbedtls::{Certificates, Session};
+
+const SSID: &str = env!("SSID");
+const PASSWORD: &str = env!("PASSWORD");
+const CERT: &'static str = concat!(include_str!("../certs/AmazonRootCA1.pem"), "\0");
+const CLIENT_CERT: &'static str = concat!(include_str!("../certs/device-certificate.pem.crt"), "\0");
+const PRIVATE_KEY: &'static str = concat!(include_str!("../certs/private.pem.key"), "\0");
 
 fn make_bits(bytes :&[u8]) -> u32 {
     ((bytes[0] as u32) << 24)
@@ -41,23 +64,9 @@ fn main() -> ! {
     let peripherals = Peripherals::take();
     let mut system = peripherals.SYSTEM.split();
     let clocks = ClockControl::configure(system.clock_control, CpuClock::Clock240MHz).freeze();
-
-    let timer_group0 = TimerGroup::new(peripherals.TIMG1, &clocks,  &mut system.peripheral_clock_control);
-    let timer = timer_group0.timer0;
-
-    initialize(
-        timer,
-        Rng::new(peripherals.RNG),
-        system.radio_clock_control,
-        &clocks,
-    )
-    .unwrap();
-
-    let mut wdt = timer_group0.wdt;
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
 
     // Disable the RTC and TIMG watchdog timers
-    wdt.disable();
     rtc.rwdt.disable();
     
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
@@ -67,8 +76,6 @@ fn main() -> ! {
     let mut backlight = io.pins.gpio45.into_push_pull_output();
 
     backlight.set_high().unwrap();
-
-    
 
     let spi = spi::Spi::new_no_cs_no_miso(
         peripherals.SPI2,
@@ -96,37 +103,98 @@ fn main() -> ! {
     build_ui(&mut display);
 
     let (wifi, _) = peripherals.RADIO.split();
-    let mut esp_now = esp_wifi::esp_now::EspNow::new(wifi).unwrap();
-    println!("esp-now version {}", esp_now.get_version().unwrap());
+    let mut socket_set_entries: [SocketStorage; 3] = Default::default();
+    let (iface, device, mut controller, sockets) =
+        create_network_interface(wifi, WifiMode::Sta, &mut socket_set_entries);
+    let wifi_stack = WifiStack::new(iface, device, sockets, current_millis);
 
-    let mut next_send_time = current_millis() + 5 * 1000;
-    
+    let rngp = Rng::new(peripherals.RNG);
+    let timer = TimerGroup::new(
+        peripherals.TIMG1,
+        &clocks,
+        &mut system.peripheral_clock_control,
+    );
+
+    initialize(
+        timer.timer0,
+        rngp,
+        system.radio_clock_control,
+        &clocks,
+    )
+    .unwrap();
+
+    println!("Call wifi_connect");
+    let client_config = Configuration::Client(ClientConfiguration {
+        ssid: SSID.into(),
+        password: PASSWORD.into(),
+        ..Default::default()
+    });
+
+    controller.set_configuration(&client_config).unwrap();
+    controller.start().unwrap();
+    controller.connect().unwrap();
+
+    println!("Wait to get connected");
     loop {
-        let r = esp_now.receive();
-        if let Some(r) = r {
-            let bits: u32 = make_bits(r.get_data());
-            let temperature = f32::from_bits(bits);
-            println!("Received {:.1}°C ", temperature);
-            update_temperature(&mut display, temperature);
-
-            if r.info.dst_address == BROADCAST_ADDRESS {
-                if !esp_now.peer_exists(&r.info.src_address).unwrap() {
-                    esp_now
-                        .add_peer(PeerInfo {
-                            peer_address: r.info.src_address,
-                            lmk: None,
-                            channel: None,
-                            encrypt: false,
-                        })
-                        .unwrap();
+        let res = controller.is_connected();
+        match res {
+            Ok(connected) => {
+                if connected {
+                    break;
                 }
-                esp_now.send(&r.info.src_address, b"Received, Thanks!").unwrap();
+            }
+            Err(err) => {
+                println!("{:?}", err);
+                loop {}
             }
         }
-        if current_millis() >= next_send_time {
-            next_send_time = current_millis() + 5 * 5000;
-            println!("Send");
-            esp_now.send(&BROADCAST_ADDRESS, b"0123456789").unwrap();
+    }
+
+    println!("Wait to get an ip address");
+    loop {
+        wifi_stack.work();
+
+        if wifi_stack.is_iface_up() {
+            println!("Got ip {:?}", wifi_stack.get_ip_info());
+            break;
         }
+    }
+
+    println!("We are connected!");
+
+    println!("Making HTTP request");
+    let mut rx_buffer = [0u8; 1536];
+    let mut tx_buffer = [0u8; 1536];
+    let mut socket = wifi_stack.get_socket(&mut rx_buffer, &mut tx_buffer);
+
+    socket.work();
+
+    socket
+        .open(IpAddress::Ipv4(Ipv4Address::new(52, 28, 41, 87)), 8883)
+        .unwrap();
+
+    let certificates = Certificates {
+        certs: Some(CERT),
+        client_cert: Some(CLIENT_CERT),
+        client_key: Some(PRIVATE_KEY),
+        password: None,
+    };
+
+    let tls = Session::new(
+        socket,
+        "a3j3y1mdtdmkz5-ats.iot.eu-central-1.amazonaws.com",
+        Mode::Client,
+        TlsVersion::Tls1_2,
+        certificates,
+    )
+    .unwrap();
+
+    println!("Start tls connect");
+    tls.connect().unwrap();
+
+    println!("Did we even get here?");
+
+    loop {
+
     }
 }
